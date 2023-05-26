1f3332c5f929f5586add96f5b34cc64b0cbda990
Rilee-0320 <go13209@naver.com>
2023-05-26 16:12:41 +0900

    [FEAT] basic login

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
 from django.contrib.auth import login as auth_login
+from django.contrib.auth.forms import AuthenticationForm
 
 
 # Create your views here.
 def signup(request):
+    if request.user.is_authenticated:
+        return redirect('mountains:index')
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
@@ -17,3 +20,19 @@ def signup(request):
         'form': form
     }
     return render(request, 'accounts/signup.html', context)
+
+
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('mountains:index')
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('mountains:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/login.html', context)
