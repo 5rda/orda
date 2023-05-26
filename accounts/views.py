80eb78415e6db34c01889bd205e1b8df6978342f
Rilee-0320 <go13209@naver.com>
2023-05-26 16:33:05 +0900

    [FEAT] logout

--- accounts/views.py
+++ accounts/views.py
@@ -1,25 +1,9 @@
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
 from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.forms import AuthenticationForm
-
-
-# Create your views here.
-def signup(request):
-    if request.user.is_authenticated:
-        return redirect('mountains:index')
-    if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            auth_login(request, user)
-            return redirect('mountains:index')
-    else:
-        form = CustomUserCreationForm
-    context = {
-        'form': form
-    }
-    return render(request, 'accounts/signup.html', context)
+from django.contrib.auth.decorators import login_required
 
 
 def login(request):
@@ -36,3 +20,26 @@ def login(request):
         'form': form,
     }
     return render(request, 'accounts/login.html', context)
+
+
+@login_required
+def logout(request):
+    auth_logout(request)
+    return redirect('mountains:index')
+
+
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect('mountains:index')
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('mountains:index')
+    else:
+        form = CustomUserCreationForm
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/signup.html', context)
