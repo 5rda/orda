68bbeb12b3d1e2e57b961fc8c6296ddda7b49274
Hanui Jang <hany0147@naver.com>
2023-05-26 10:24:02 +0900

    [INIT] app

--- pjt/settings.py
+++ pjt/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'accounts',
+    'mountains',
+    'posts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
