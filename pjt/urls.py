09d3c234234bcab25ce406f35900522079a4054e
Lim Seongjoo <afro1662@naver.com>
2023-05-27 12:40:22 +0900

    [FEAT] posts

--- pjt/urls.py
+++ pjt/urls.py
@@ -17,8 +17,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from ckeditor_uploader import views as views_ckeditor
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import never_cache
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('posts/', include('posts.urls')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
+    path(r'^upload/', login_required(views_ckeditor.upload), name='ckeditor_upload'),
+    path(r'^browse/', never_cache(login_required(views_ckeditor.browse)), name='ckeditor_browse'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
