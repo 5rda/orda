e9ef34912d58780f022a39a3f20319aa652b4fef
Hanui Jang <hany0147@naver.com>
2023-05-31 14:27:15 +0900

    [ADD] mountains CRUD

--- accounts/models.py
+++ accounts/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     username = models.CharField(max_length=50, unique=True)
-    password = models.CharField(max_length=50)
+    password = models.CharField(max_length=256)
     nickname = models.CharField(max_length=20, unique=True)
     email = models.CharField(max_length=50, blank=True)
     profile_img = models.ImageField(blank=True)
