57cbfc25810ac815bb225413af8e8fc404b62c22
Hanui Jang <hany0147@naver.com>
2023-06-01 16:37:16 +0900

    [UPDATE] courses

--- mountains/models.py
+++ mountains/models.py
@@ -1,21 +1,27 @@
 # from django.db import models
 from django.contrib.gis.db import models
-from django.db.models import Sum
-from django.contrib.gis.db.models.functions import Length, Transform
+from django.conf import settings
+from django.core.serializers.json import DjangoJSONEncoder
+import json
 
 # Create your models here.
 class Mountain(models.Model):
+
+    def image_path(instance, filename):
+        return f'mountains/{instance.name}/{filename}'
+    
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50 , unique=True)
     subname = models.CharField(max_length=200)
-    height = models.FloatField()
+    height = models.IntegerField()
     region = models.CharField(max_length=100)
     diff = models.CharField(max_length=30)
     geom = models.GeometryField()
+    image = models.ImageField(upload_to=image_path, blank=True)
 
     class Meta:
         managed = False
-        db_table = 'mountains'
+        db_table = 'mountains_mountain'
 
     def __str__(self):
         return self.name
@@ -26,14 +32,16 @@ class Course(models.Model):
     crs_name = models.CharField(max_length=100, unique=True)
     distance = models.IntegerField(db_column='total_distance_int')
     duration = models.DurationField(db_column='total_duration_int')
+    bookmarks = models.ManyToManyField(settings.AUTH_USER_MODEL,  related_name='bookmarked',  db_table='mountains_course_bookmarks')
 
     class Meta:
         managed = False
-        db_table = 'course'
+        db_table = 'mountains_course'
+        ordering = ['id']
 
     def __str__(self):
         return self.crs_name
-
+    
 
 class CourseDetail(models.Model):
     id = models.AutoField(primary_key=True)
@@ -44,11 +52,28 @@ class CourseDetail(models.Model):
 
     class Meta:
         managed = False
-        db_table = 'course_detail'
+        db_table = 'mountains_coursedetail'
+        ordering = ['id']
 
     def __str__(self):
         return str(self.crs_name)
     
+    # def as_dict(self):
+    #     return {
+    #         'crs_name': self.crs_name,
+    #         'x': self.geom.x if self.geom else None,
+    #         'y': self.geom.y if self.geom else None,
+    #         'z': self.geom.z if self.geom else None,
+    #     }
+    
 
 class Comment(models.Model):
     pass
+
+
+# class CourseBookmark(models.Model):
+#     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+#     course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+#     class Meta:
+#         db_table = 'mountains_course_bookmarks'
--- mountains/urls.py
+++ mountains/urls.py
@@ -3,8 +3,6 @@ from .views import *
 
 app_name = 'mountains'
 urlpatterns = [
-    path('mountain_list/', MountainListView.as_view(), name='mountain_list'),
+    path('', MountainListView.as_view(), name='mountain_list'),
     path('<int:pk>/', MountainDetailView.as_view(), name='mountain_detail'),
-    path('<int:mountain_pk>/courses/', CourseListView.as_view(), name='course_detail'),
-
 ]
