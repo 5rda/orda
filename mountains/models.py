5f0192c023989b7140c39a216764249b7f439ca9
Rilee-0320 <go13209@naver.com>
2023-06-02 11:28:34 +0900

    [FEAT] mountains_review likes

--- mountains/models.py
+++ mountains/models.py
@@ -89,7 +89,7 @@ class Review(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     tags = models.CharField(max_length=200)
-
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_reviews')
 
     def __str__(self):
         return self.content
