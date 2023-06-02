e598584ac72cc04df15a50c2281751235a28a4f8
Hanui Jang <120097271+hany0147@users.noreply.github.com>
2023-06-02 09:40:59 +0900

    Update urls.py
    - view가 이미 임포트 되어있어서 수정했습니다.

--- mountains/urls.py
+++ mountains/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 from .views import *
-from . import views
 
 app_name = 'mountains'
 urlpatterns = [
     path('', MountainListView.as_view(), name='mountain_list'),
     path('<int:pk>/', MountainDetailView.as_view(), name='mountain_detail'),
-    path('<int:pk>/create_review/', views.create_review, name='create_review'),
+    path('<int:pk>/create_review/', create_review, name='create_review'),
 
 ]
