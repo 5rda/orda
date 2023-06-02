fe781d02dc5f5d1ccde32a99b2e31532bc236de8
Rilee-0320 <go13209@naver.com>
2023-06-02 09:35:16 +0900

    [FEAT] review_create

--- mountains/urls.py
+++ mountains/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import *
+from . import views
 
 app_name = 'mountains'
 urlpatterns = [
     path('', MountainListView.as_view(), name='mountain_list'),
     path('<int:pk>/', MountainDetailView.as_view(), name='mountain_detail'),
+    path('<int:pk>/create_review/', views.create_review, name='create_review'),
+
 ]
--- mountains/views.py
+++ mountains/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import DetailView, ListView
 from django.contrib.gis.serializers.geojson import Serializer
 from django.shortcuts import get_object_or_404
@@ -6,6 +6,7 @@ from .models import *
 import json, os
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
+from .forms import ReviewCreationForm
 # Create your views here.
 
 def index(request):
@@ -43,3 +44,54 @@ class MountainDetailView(DetailView):
         # with open(file_path, 'w', encoding='utf-8') as file:
         #     file.write(json_data)
         return context
+
+
+def create_review(request, pk):
+    mountain = Mountain.objects.get(pk=pk)
+    TAG_LIST = [
+        ('풍경이 아름다운', 1),
+        ('장엄한', 2),
+        ('설산이 아름다운', 3),
+        ('단풍이 아름다운', 4),
+        ('계곡이 있는', 5),
+        ('바다가 보이는', 6),
+        ('사진 찍기 좋은', 7),
+        ('일출 명소', 8),
+        ('약수물 맛이 좋은', 9),
+        ('어르신과 함께', 10),
+        ('아이와 함께', 11),
+        ('반려견과 함께', 12),
+        ('커플 끼리', 13),
+        ('경사가 낮은', 14),
+        ('등산 초보', 15),
+        ('위험한', 16),
+        ('트래킹 수준인', 17),
+        ('야생 동물 출몰', 18),
+        ('화장실이 많은', 19),
+        ('관리가 잘된', 20),
+        ('주차가 편한', 21),
+        ('관광 명소가 많은', 22),
+        ('맛집 꿀맛', 23),
+        ('백패킹 명소', 24),
+        ('야간산행하기 좋은', 25),
+        ('주말등산', 26)
+    ]
+
+    if request.method == 'POST':
+        form = ReviewCreationForm(request.POST, request.FILES)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.mountain = mountain
+            review.user = request.user
+            tags = request.POST.getlist('tags')
+            review.tags = list(map(int, tags))
+            review.save()
+            return redirect('accounts:profile', request.user.pk)
+    else:
+        form = ReviewCreationForm()
+    context = {
+        'form': form,
+        'tag_list': TAG_LIST,
+        'mountain': mountain,
+    }
+    return render(request, 'mountains/create_review.html', context)
