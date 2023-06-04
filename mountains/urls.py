c5df61dbef118673b2c9fb2a59a41d594df25be7
Hanui Jang <hany0147@naver.com>
2023-06-04 23:05:30 +0900

    [UPDATE] Courses, Mountains DB

--- mountains/urls.py
+++ mountains/urls.py
@@ -6,10 +6,12 @@ urlpatterns = [
     path('', MountainListView.as_view(), name='mountain_list'),
     path('<int:pk>/', MountainDetailView.as_view(), name='mountain_detail'),
     path('<int:mountain_pk>/likes/', mountain_likes, name='mountain_likes'),
+    path('<int:mountain_pk>/courses/', CourseListView.as_view(), name='course_list'),
     path('<int:mountain_pk>/course/<int:course_pk>/bookmark/', bookmark, name='bookmark'),
-    path('courses/', CourseListView.as_view(), name='course_list'),
+    path('courses/', CourseAllListView.as_view(), name='course_all_list'),
     path('<int:pk>/create_review/', create_review, name='create_review'),
     path('<int:pk>/review_likes/<int:review_pk>/', review_likes, name='review_likes'),
+    path('mountain_list/', MountainTestView.as_view(), name='mountain_test'),
     
 
 ]
