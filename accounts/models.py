7db1cc8f969a35672c8bab27fc7ae6634f78817e
Lim Seongjoo <afro1662@naver.com>
2023-06-08 16:04:23 +0900

    [FEAT] notification

--- accounts/models.py
+++ accounts/models.py
@@ -1,22 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from mountains.models import Mountain, Course
-
+from django.urls import reverse
 
 class User(AbstractUser):
     username = models.CharField(max_length=50, unique=True)
     password = models.CharField(max_length=256)
-    nickname = models.CharField(max_length=20, unique=True, blank=True, null=True)
-    email = models.CharField(max_length=50, blank=True, null=True)
-    profile_img = models.ImageField(blank=True, null=True)
+    nickname = models.CharField(max_length=20, unique=True)
+    email = models.CharField(max_length=50, blank=True)
+    profile_img = models.ImageField(blank=True)
     joined_at = models.DateTimeField(auto_now_add=True)
     followings = models.ManyToManyField('self', symmetrical=False, related_name='followers')
     message = models.CharField(max_length=200, blank=True)
     kakao_user_id = models.CharField(max_length=50, unique=True, blank=True, null=True)
     naver_user_id = models.CharField(max_length=50, unique=True, blank=True, null=True)
     visited_courses = models.ManyToManyField(Course, through='VisitedCourse', related_name='visitors', blank=True)
-    level = models.IntegerField(default=1)
-
+        
+    def check_notifications(self):
+        unchecked_notifications = self.notifications.filter(is_checked=False)
+        unchecked_notifications.update(is_checked=True)
+        return unchecked_notifications
 
 class VisitedCourse(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -31,3 +34,31 @@ class VisitedCourse(models.Model):
         self.mountain_name = self.course.mntn_name
         self.mountain_id = self.course.mntn_name.id
         super().save(*args, **kwargs)
+        
+class Notification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
+    notification_type = models.CharField(max_length=50)
+    post = models.ForeignKey('posts.Post', on_delete=models.CASCADE, blank=True, null=True)
+    message = models.TextField()
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def get_notification_url(self):
+        if self.notification_type == '댓글' and self.post:
+            return reverse('posts:detail', kwargs={'post_pk': self.post.pk})
+        else:
+            return reverse('accounts:profile', kwargs={'pk': self.user.pk})
+    
+    def __str__(self):
+        return self.message
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
