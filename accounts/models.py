7fa03cd15a7ba654898fdc4f2ed4d4502fa30f25
Lim Seongjoo <afro1662@naver.com>
2023-06-08 21:12:43 +0900

    [FIX] notifications

--- accounts/models.py
+++ accounts/models.py
@@ -3,13 +3,12 @@ from django.contrib.auth.models import AbstractUser
 from mountains.models import Mountain, Course
 from django.urls import reverse
 
-
 class User(AbstractUser):
     username = models.CharField(max_length=50, unique=True)
     password = models.CharField(max_length=256)
-    nickname = models.CharField(max_length=20, unique=True)
-    email = models.CharField(max_length=50, blank=True)
-    profile_img = models.ImageField(blank=True)
+    nickname = models.CharField(max_length=20, unique=True, blank=True, null=True)
+    email = models.CharField(max_length=50, blank=True, null=True)
+    profile_img = models.ImageField(blank=True, null=True)
     joined_at = models.DateTimeField(auto_now_add=True)
     followings = models.ManyToManyField('self', symmetrical=False, related_name='followers')
     message = models.CharField(max_length=200, blank=True)
@@ -17,13 +16,11 @@ class User(AbstractUser):
     naver_user_id = models.CharField(max_length=50, unique=True, blank=True, null=True)
     visited_courses = models.ManyToManyField(Course, through='VisitedCourse', related_name='visitors', blank=True)
     level = models.IntegerField(default=1)
-    
-        
+
     def check_notifications(self):
         unchecked_notifications = self.notifications.filter(is_checked=False)
         unchecked_notifications.update(is_checked=True)
         return unchecked_notifications
-
 
 class VisitedCourse(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
