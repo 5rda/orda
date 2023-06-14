7d874d84ee7947d2be35258d7fa0f873b755c706
Hanui Jang <hany0147@naver.com>
2023-06-14 11:23:02 +0900

    [ADD] env

--- static/js/accounts_mymemories_map.js
+++ static/js/accounts_mymemories_map.js
@@ -4,9 +4,10 @@
         callback();
     } else {
         // 카카오맵 API 로드
+        const API_KEY = config.apikey;
         var script = document.createElement("script");
         script.type = "text/javascript";
-        script.src = "https://dapi.kakao.com/v2/maps/sdk.js?appkey=d39fcd60ffdc96ae99454337f5e45a16&libraries=services&autoload=false";
+        script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${API_KEY}&libraries=services&autoload=false`;
         document.head.appendChild(script);
         
         script.onload = function () {
