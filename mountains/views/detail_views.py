1f31347884770025ba23834eb8d687c932b109ea
Hanui Jang <hany0147@naver.com>
2023-06-15 11:17:55 +0900

    [TEST] weather api

--- mountains/views/detail_views.py
+++ mountains/views/detail_views.py
@@ -41,18 +41,18 @@ class MountainDetailView(LoginRequiredMixin, DetailView):
         most_liked_review = reviews.annotate(num_likes=Count('like_users')).order_by('-num_likes').first()
 
         # 기타
-        now_weather_data = self.get_weather_forecast()
+        # now_weather_data = self.get_weather_forecast()
 
-        tem = now_weather_data['기온']
-        hum = now_weather_data['습도']
-        sky = now_weather_data['하늘상태']
-        rain = now_weather_data['강수량']
-        vec = now_weather_data['풍향']
-        wsd = now_weather_data['풍속']
-        now_time = now_weather_data['현재시각']
+        # tem = now_weather_data['기온']
+        # hum = now_weather_data['습도']
+        # sky = now_weather_data['하늘상태']
+        # rain = now_weather_data['강수량']
+        # vec = now_weather_data['풍향']
+        # wsd = now_weather_data['풍속']
+        # now_time = now_weather_data['현재시각']
 
-        sun = ['0700', '0800', '0900', '1000', '1100', '1200', '1300', '1400', '1500', '1600', '1700', '1800', '1900']
-        moon = ['2000', '2100', '2200', '2300', '0000', '0100', '0200', '0300', '0400', '0500', '0600']
+        # sun = ['0700', '0800', '0900', '1000', '1100', '1200', '1300', '1400', '1500', '1600', '1700', '1800', '1900']
+        # moon = ['2000', '2100', '2200', '2300', '0000', '0100', '0200', '0300', '0400', '0500', '0600']
         
         air_data = self.get_air()
 
@@ -124,15 +124,15 @@ class MountainDetailView(LoginRequiredMixin, DetailView):
             'most_liked_review': most_liked_review,
 
             # 날씨
-            'tem': tem,
-            'hum': hum,
-            'sky': sky,
-            'rain': rain,
-            'vec': vec,
-            'wsd': wsd,
-            'now_time': now_time,
-            'sun': sun,
-            'moon': moon,
+            # 'tem': tem,
+            # 'hum': hum,
+            # 'sky': sky,
+            # 'rain': rain,
+            # 'vec': vec,
+            # 'wsd': wsd,
+            # 'now_time': now_time,
+            # 'sun': sun,
+            # 'moon': moon,
 
             # 미세먼지, 오존
             'region': region,
@@ -176,145 +176,145 @@ class MountainDetailView(LoginRequiredMixin, DetailView):
             'result': result,
         }
     
-    def get_weather_forecast(self):
-        mountain = self.get_object()
-        # API 요청을 위한 URL과 파라미터 설정
-        url = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst"
+    # def get_weather_forecast(self):
+    #     mountain = self.get_object()
+    #     # API 요청을 위한 URL과 파라미터 설정
+    #     url = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst"
 
-        serviceKey = os.environ['WEATHER_KEY']
-        serviceKeyDecoded = unquote(serviceKey, 'UTF-8')
+    #     serviceKey = os.environ['WEATHER_KEY']
+    #     serviceKeyDecoded = unquote(serviceKey, 'UTF-8')
 
-        now = datetime.now()
-        today = datetime.today().strftime("%Y%m%d")
-        y = date.today() - timedelta(days=1)
-        yesterday = y.strftime("%Y%m%d")
+    #     now = datetime.now()
+    #     today = datetime.today().strftime("%Y%m%d")
+    #     y = date.today() - timedelta(days=1)
+    #     yesterday = y.strftime("%Y%m%d")
 
-        NX = 149            ## X축 격자점 수
-        NY = 253            ## Y축 격자점 수
+    #     NX = 149            ## X축 격자점 수
+    #     NY = 253            ## Y축 격자점 수
 
-        Re = 6371.00877     ##  지도반경
-        grid = 5.0          ##  격자간격 (km)
-        slat1 = 30.0        ##  표준위도 1
-        slat2 = 60.0        ##  표준위도 2
-        olon = 126.0        ##  기준점 경도
-        olat = 38.0         ##  기준점 위도
-        xo = 210 / grid     ##  기준점 X좌표
-        yo = 675 / grid     ##  기준점 Y좌표
-        first = 0
+    #     Re = 6371.00877     ##  지도반경
+    #     grid = 5.0          ##  격자간격 (km)
+    #     slat1 = 30.0        ##  표준위도 1
+    #     slat2 = 60.0        ##  표준위도 2
+    #     olon = 126.0        ##  기준점 경도
+    #     olat = 38.0         ##  기준점 위도
+    #     xo = 210 / grid     ##  기준점 X좌표
+    #     yo = 675 / grid     ##  기준점 Y좌표
+    #     first = 0
 
-        if first == 0 :
-            PI = math.asin(1.0) * 2.0
-            DEGRAD = PI/ 180.0
+    #     if first == 0 :
+    #         PI = math.asin(1.0) * 2.0
+    #         DEGRAD = PI/ 180.0
             
-            re = Re / grid
-            slat1 = slat1 * DEGRAD
-            slat2 = slat2 * DEGRAD
-            olon = olon * DEGRAD
-            olat = olat * DEGRAD
+    #         re = Re / grid
+    #         slat1 = slat1 * DEGRAD
+    #         slat2 = slat2 * DEGRAD
+    #         olon = olon * DEGRAD
+    #         olat = olat * DEGRAD
 
-            sn = math.tan(PI * 0.25 + slat2 * 0.5) / math.tan(PI * 0.25 + slat1 * 0.5)
-            sn = math.log(math.cos(slat1) / math.cos(slat2)) / math.log(sn)
-            sf = math.tan(PI * 0.25 + slat1 * 0.5)
-            sf = math.pow(sf, sn) * math.cos(slat1) / sn
-            ro = math.tan(PI * 0.25 + olat * 0.5)
-            ro = re * sf / math.pow(ro, sn)
-            first = 1
+    #         sn = math.tan(PI * 0.25 + slat2 * 0.5) / math.tan(PI * 0.25 + slat1 * 0.5)
+    #         sn = math.log(math.cos(slat1) / math.cos(slat2)) / math.log(sn)
+    #         sf = math.tan(PI * 0.25 + slat1 * 0.5)
+    #         sf = math.pow(sf, sn) * math.cos(slat1) / sn
+    #         ro = math.tan(PI * 0.25 + olat * 0.5)
+    #         ro = re * sf / math.pow(ro, sn)
+    #         first = 1
 
-        def mapToGrid(lat, lon, code = 0 ):
-            ra = math.tan(PI * 0.25 + lat * DEGRAD * 0.5)
-            ra = re * sf / pow(ra, sn)
-            theta = lon * DEGRAD - olon
-            if theta > PI :
-                theta -= 2.0 * PI
-            if theta < -PI :
-                theta += 2.0 * PI
-            theta *= sn
-            x = (ra * math.sin(theta)) + xo
-            y = (ro - ra * math.cos(theta)) + yo
-            x = int(x + 1.5)
-            y = int(y + 1.5)
-            return x, y
+    #     def mapToGrid(lat, lon, code = 0 ):
+    #         ra = math.tan(PI * 0.25 + lat * DEGRAD * 0.5)
+    #         ra = re * sf / pow(ra, sn)
+    #         theta = lon * DEGRAD - olon
+    #         if theta > PI :
+    #             theta -= 2.0 * PI
+    #         if theta < -PI :
+    #             theta += 2.0 * PI
+    #         theta *= sn
+    #         x = (ra * math.sin(theta)) + xo
+    #         y = (ro - ra * math.cos(theta)) + yo
+    #         x = int(x + 1.5)
+    #         y = int(y + 1.5)
+    #         return x, y
         
-        nx, ny = mapToGrid(mountain.geom.y, mountain.geom.x)
+    #     nx, ny = mapToGrid(mountain.geom.y, mountain.geom.x)
 
-        if 0 < now.minute <= 59: # base_time와 base_date 구하는 함수
-            if now.hour==0:
-                base_time = "2330"
-                base_date = yesterday
-            else:
-                pre_hour = now.hour-1
-                if pre_hour < 10:
-                    base_time = "0" + str(pre_hour) + "30"
-                else:
-                    base_time = str(pre_hour) + "30"
-                base_date = today
-        else:
-            if now.hour < 10:
-                base_time = "0" + str(now.hour-1) + "30"
-            else:
-                base_time = str(now.hour-1) + "30"
-            base_date = today
+    #     if 0 < now.minute <= 59: # base_time와 base_date 구하는 함수
+    #         if now.hour==0:
+    #             base_time = "2330"
+    #             base_date = yesterday
+    #         else:
+    #             pre_hour = now.hour-1
+    #             if pre_hour < 10:
+    #                 base_time = "0" + str(pre_hour) + "30"
+    #             else:
+    #                 base_time = str(pre_hour) + "30"
+    #             base_date = today
+    #     else:
+    #         if now.hour < 10:
+    #             base_time = "0" + str(now.hour-1) + "30"
+    #         else:
+    #             base_time = str(now.hour-1) + "30"
+    #         base_date = today
 
-        if now.hour < 10:
-            now_time = '0'+str(now.hour)+'0'+'0'
-        else:
-            now_time = str(now.hour)+'0'+'0'
+    #     if now.hour < 10:
+    #         now_time = '0'+str(now.hour)+'0'+'0'
+    #     else:
+    #         now_time = str(now.hour)+'0'+'0'
 
-        queryParams = '?' + urlencode({ 
-              quote_plus('serviceKey') : serviceKeyDecoded,
-              quote_plus('base_date') : base_date,
-              quote_plus('base_time') : base_time,
-              quote_plus('nx') : nx,
-              quote_plus('ny') : ny,
-              quote_plus('dataType') : 'json',
-              quote_plus('numOfRows') : '1000'
-              })
+    #     queryParams = '?' + urlencode({ 
+    #           quote_plus('serviceKey') : serviceKeyDecoded,
+    #           quote_plus('base_date') : base_date,
+    #           quote_plus('base_time') : base_time,
+    #           quote_plus('nx') : nx,
+    #           quote_plus('ny') : ny,
+    #           quote_plus('dataType') : 'json',
+    #           quote_plus('numOfRows') : '1000'
+    #           })
 
-        # API 요청 보내기
-        response = requests.get(url + queryParams, verify=False)
-        items = response.json().get('response').get('body').get('items') #데이터들 아이템에 저장
-        now_weather_data = dict()
+    #     # API 요청 보내기
+    #     response = requests.get(url + queryParams, verify=False)
+    #     items = response.json().get('response').get('body').get('items') #데이터들 아이템에 저장
+    #     now_weather_data = dict()
     
-        for item in items['item']:
-            # 기온
-            if item['category'] == 'T1H' and item['fcstDate'] == today and item['fcstTime'] == now_time:
-                now_weather_data['기온'] = item['fcstValue']
-            # 습도
-            if item['category'] == 'REH' and item['fcstDate'] == today and item['fcstTime'] == now_time:
-                now_weather_data['습도'] = item['fcstValue']
-            # 하늘상태: 맑음(1) 구름많은(3) 흐림(4)
-            if item['category'] == 'SKY' and item['fcstDate'] == today and item['fcstTime'] == now_time:
-                now_weather_data['하늘상태'] = item['fcstValue']
-            # 1시간 동안 강수량
-            if item['category'] == 'RN1' and item['fcstDate'] == today and item['fcstTime'] == now_time:
-                now_weather_data['강수량'] = item['fcstValue']
-            # 풍향
-            if item['category'] == 'VEC' and item['fcstDate'] == today and item['fcstTime'] == now_time:
-                def get_direction(deg):
-                    if '22.5' <= deg < '67.5':
-                        return '북동'
-                    elif '67.5' <= deg < '112.5':
-                        return '동'
-                    elif '112.5' <= deg < '157.5':
-                        return '남동'
-                    elif '157.5' <= deg < '202.5':
-                        return '남'
-                    elif '202.5' <= deg < '247.5':
-                        return '남서'
-                    elif '247.5' <= deg < '292.5':
-                        return '서'
-                    elif '292.5' <= deg < '337.5':
-                        return '북서'
-                    else:
-                        return '북'
-                now_weather_data['풍향'] = get_direction(item['fcstValue'])
-            # 풍속
-            if item['category'] == 'WSD' and item['fcstDate'] == today and item['fcstTime'] == now_time:
-                now_weather_data['풍속'] = item['fcstValue']
-            # 현재시각
-            if item['fcstDate'] == today and item['fcstTime'] == now_time:
-                now_weather_data['현재시각'] = now_time
-        return now_weather_data
+    #     for item in items['item']:
+    #         # 기온
+    #         if item['category'] == 'T1H' and item['fcstDate'] == today and item['fcstTime'] == now_time:
+    #             now_weather_data['기온'] = item['fcstValue']
+    #         # 습도
+    #         if item['category'] == 'REH' and item['fcstDate'] == today and item['fcstTime'] == now_time:
+    #             now_weather_data['습도'] = item['fcstValue']
+    #         # 하늘상태: 맑음(1) 구름많은(3) 흐림(4)
+    #         if item['category'] == 'SKY' and item['fcstDate'] == today and item['fcstTime'] == now_time:
+    #             now_weather_data['하늘상태'] = item['fcstValue']
+    #         # 1시간 동안 강수량
+    #         if item['category'] == 'RN1' and item['fcstDate'] == today and item['fcstTime'] == now_time:
+    #             now_weather_data['강수량'] = item['fcstValue']
+    #         # 풍향
+    #         if item['category'] == 'VEC' and item['fcstDate'] == today and item['fcstTime'] == now_time:
+    #             def get_direction(deg):
+    #                 if '22.5' <= deg < '67.5':
+    #                     return '북동'
+    #                 elif '67.5' <= deg < '112.5':
+    #                     return '동'
+    #                 elif '112.5' <= deg < '157.5':
+    #                     return '남동'
+    #                 elif '157.5' <= deg < '202.5':
+    #                     return '남'
+    #                 elif '202.5' <= deg < '247.5':
+    #                     return '남서'
+    #                 elif '247.5' <= deg < '292.5':
+    #                     return '서'
+    #                 elif '292.5' <= deg < '337.5':
+    #                     return '북서'
+    #                 else:
+    #                     return '북'
+    #             now_weather_data['풍향'] = get_direction(item['fcstValue'])
+    #         # 풍속
+    #         if item['category'] == 'WSD' and item['fcstDate'] == today and item['fcstTime'] == now_time:
+    #             now_weather_data['풍속'] = item['fcstValue']
+    #         # 현재시각
+    #         if item['fcstDate'] == today and item['fcstTime'] == now_time:
+    #             now_weather_data['현재시각'] = now_time
+    #     return now_weather_data
 
     def get_air(self):
         mountain = self.get_object()
