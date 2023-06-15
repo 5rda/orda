e5cb1d6973f5cbc8ef47753d92f34ae5a6280ef3
Hanui Jang <hany0147@naver.com>
2023-06-15 12:33:37 +0900

    .

--- pjt/settings.py
+++ pjt/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['orda.duckdns.org', 'localhost']
 
 # Application definition
 
